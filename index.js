7d966970c043121a27f6e3a5eddd38088475f29f
PromiSe#### <vicimpa@ya.ru>
2019-08-06 14:41:24 +0300

    Update module

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
 Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./api"));
-__export(require("./configuration"));
+__export(require("./api.js"));
+__export(require("./configuration.js"));
